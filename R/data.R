05905e5c285bf661d269e2cdcc6d82a26e37c1d6
francoiskroll <francois@kroll.be>
2022-11-14 19:46:09 +0000

    added data: allparameters & alluparams

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' All Parameters
+#'
+#' Simple vector with all parameter names, e.g. activeboutMean.
+#'
+#'
+#' @format ## `activityPercentageTimeActivy, activityTotalPx, ...`
+#' A vector of strings:
+#' \describe{
+#'   \item{allparameters}{All parameters}
+#' }
+#' @source
+"allparameters"
+
+#####
+
+#' All Unique Parameters
+#'
+#' Simple vector with all unique parameter names, e.g. day_activeboutMean.
+#'
+#'
+#' @format ## `day_activityPercentageTimeActive, day_activityTotalPx, ...`
+#' A vector of strings:
+#' \describe{
+#'   \item{alluparams}{All unique parameters}
+#' }
+#' @source
+"alluparams"
